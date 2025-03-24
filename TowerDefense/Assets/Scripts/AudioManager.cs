563f8530771ccf554e0986816291511d0c04b170
Yeyolive <128828380+Yeyolive@users.noreply.github.com>
2025-03-24 18:57:39 -0500

    Manages or controls  the Audio

---
+++ TowerDefense/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager Instance;
+
+    public AudioClip turretFireClip;
+    public AudioClip explosionClip;
+    public AudioClip enemyDetectedClip;
+    public AudioClip reloadClip;
+    public AudioClip backgroundMusicClip;
+    public AudioClip laserClip;
+
+    private AudioSource sfxSource;
+    private AudioSource musicSource;
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+
+            sfxSource = gameObject.AddComponent<AudioSource>();
+            musicSource = gameObject.AddComponent<AudioSource>();
+            musicSource.loop = true;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void Start()
+    {
+        PlayBackgroundMusic();
+    }
+
+    public void PlayTurretFire() => sfxSource.PlayOneShot(turretFireClip);
+    public void PlayExplosion() => sfxSource.PlayOneShot(explosionClip);
+    public void PlayEnemyDetected() => sfxSource.PlayOneShot(enemyDetectedClip);
+    public void PlayReload() => sfxSource.PlayOneShot(reloadClip);
+    public void PlayLaser() => sfxSource.PlayOneShot(laserClip);
+
+    public void PlayBackgroundMusic()
+    {
+        if (backgroundMusicClip != null)
+        {
+            musicSource.clip = backgroundMusicClip;
+            musicSource.Play();
+        }
+    }
+}
