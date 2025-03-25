ac08828e1a5e6a0e1036b892a8d96c502addbdb2
Yeyolive <128828380+Yeyolive@users.noreply.github.com>
2025-03-24 19:00:27 -0500

    turret fire sound trigger

--- TowerDefense/Assets/Scripts/Turret.cs
+++ TowerDefense/Assets/Scripts/Turret.cs
@@ -86,6 +86,8 @@ public class Turret : MonoBehaviour
                 }
             }
             timeOfLastAttack = Time.time;
+
+            AudioManager.Instance.PlayTurretFire(); 
         }
     }
 
